15ca1cb5dfbb77f6eccd6e090aa132068e95a416
Schuttler <schuttlerc@outlook.com>
2023-07-20 03:29:39 -0500

    Update Homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,55 @@
 import os
 import csv
 csvpath = os.path.join('Resources/budget_data.csv')
-with open(csvpath, 'r', encoding = 'utf-8') as csvfile:
+
+rows = []
+date = []
+profits = []
+
+with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
     csv_header = next(csvreader)
-    print(f'CSV Header: {csv_header}')
+
     for row in csvreader:
-        print(row)  
+        rows.append(row)
+        date.append(row[0])
+        profits.append(row[1])
+
+profits_int = [eval(i) for i in profits]
+    
+total_months = len(rows)
+net_total = sum(profits_int)
+
+change =[profits_int[i +1]-profits_int[i] for i in range(total_months - 1)]
+average_change = sum(change) / len(change)
+
+greatest_increase = max(change)
+greatest_decrease = min(change)
+
+gi_index = change.index(greatest_increase)
+gd_index = change.index(greatest_decrease)
+gi_date = date[gi_index+1]
+gd_date = date[gd_index+1]
+
+net_total = "${:,}".format(net_total)
+average_change = "${:,.2f}".format(average_change)
+greatest_increase = "(${:,})".format(greatest_increase)
+greatest_decrease = "(${:,})".format(greatest_decrease)
+
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months:", total_months)
+print("Total:", net_total)
+print("Average Change:", average_change)
+print("Greatest Increase in Profits:", gi_date, greatest_increase)
+print("Greatest Decrease in Profits:", gd_date, greatest_decrease)
+
+out = open("analysis/output.txt", "w")
+out.write("Financial Analysis")
+out.write("\n----------------------------")
+out.write("\nTotal Months: " + str(total_months))
+out.write("\nTotal: " + str(net_total))
+out.write("\nAverage Change: " + str(average_change))
+out.write("\nGreatest Increase in Profits: " + str(gi_date) + " " + str(greatest_increase))
+out.write("\nGreatest Decrease in Profits: " + str(gd_date) + " " + str(greatest_decrease))
+out.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,76 @@
 import os
 import csv
-csvpath = os.path.join('Resources', 'budget_data.csv')
-with open(csvpath, 'r', encoding = 'utf-8') as csvfile:
+csvpath = os.path.join('Resources/election_data.csv')
+
+rows = []
+ballot_id = []
+county = []
+candidate = []
+
+with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
     csv_header = next(csvreader)
-    print(f'CSV Header: {csv_header}')
+
     for row in csvreader:
-        print(row)
+        rows.append(row)
+        ballot_id.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+
+total_votes = len(rows)
+
+set_candidate = set(candidate)
+list_candidate = list(set_candidate)
+list_candidate.sort()
+candidate1 = list_candidate[0]
+candidate2 = list_candidate[1]
+candidate3 = list_candidate[2]
+
+candidate1_count = candidate.count(candidate1)
+candidate2_count = candidate.count(candidate2)
+candidate3_count = candidate.count(candidate3)
+
+candidate1_percentage = candidate1_count / total_votes * 100
+candidate2_percentage = candidate2_count / total_votes * 100
+candidate3_percentage = candidate3_count / total_votes * 100
+
+candidates = [candidate1,candidate2,candidate3]
+candidate_counts = [candidate1_count,candidate2_count,candidate3_count]
+most_votes = max(candidate_counts)
+most_votes_index = candidate_counts.index(most_votes)
+winner = candidates[most_votes_index]
+
+total_votes = "{:,}".format(total_votes)
+candidate1 = "{}:".format(candidate1)
+candidate2 = "{}:".format(candidate2)
+candidate3 = "{}:".format(candidate3)
+candidate1_count = "({:,})".format(candidate1_count)
+candidate2_count = "({:,})".format(candidate2_count)
+candidate3_count = "({:,})".format(candidate3_count)
+candidate1_percentage = "{:.3f}%".format(candidate1_percentage)
+candidate2_percentage = "{:.3f}%".format(candidate2_percentage)
+candidate3_percentage = "{:.3f}%".format(candidate3_percentage)
+
+print("Election Results")
+print("-------------------------")
+print("Total Votes:", total_votes)
+print("-------------------------")
+print(candidate1,candidate1_percentage, candidate1_count)
+print(candidate2,candidate2_percentage, candidate2_count)
+print(candidate3,candidate3_percentage, candidate3_count)
+print("-------------------------")
+print("Winner:", winner)
+print("-------------------------")
+
+out = open("analysis/output.txt", "w")
+out.write("Election Results")
+out.write("\n-------------------------")
+out.write("\nTotal Votes: " + str(total_votes))
+out.write("\n-------------------------")
+out.write("\n" + str(candidate1) + " " + str(candidate1_percentage) + str(candidate1_count))
+out.write("\n" + str(candidate2) + " " + str(candidate2_percentage) + str(candidate2_count))
+out.write("\n" + str(candidate3) + " " + str(candidate3_percentage) + str(candidate3_count))
+out.write("\n-------------------------")
+out.write("\nWinner: " + str(winner))
+out.write("\n-------------------------")
+out.close()
